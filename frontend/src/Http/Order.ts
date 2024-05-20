788d2cb3fbea121a552c3f5f156c5b8e8fafa8a6
OverlordCrowley <maksim2004@bk.ru>
2024-05-21 00:00:13 +0500

    fix

--- frontend/src/Http/Order.ts
+++ frontend/src/Http/Order.ts
@@ -1,13 +1,13 @@
 import {$api} from './index';
 
 
-export const addOrder = async (idAddress: number, cardId: number, comment: string, timeOrder: string, bonusActive: boolean, idOrders: number[]) => {
+export const addOrder = async (idAddress: number, cardId: number, comment: string, timeOrder: string, bonus: boolean, idOrders: number[]) => {
     let res;
     if(comment){
-        res = await $api.post(`/history/order/add`,{idAddress: idAddress, cardId: cardId, comment: comment, timeOrder: timeOrder, bonusActive: bonusActive, idOrders: idOrders });
+        res = await $api.post(`/history/order/add`,{idAddress: idAddress, idCart: cardId, comment: comment, timeOrder: timeOrder, bonus: bonus, idOrders: idOrders });
     }
     else{
-        res = await $api.post(`/history/order/add`,{idAddress: idAddress, cardId: cardId, timeOrder: timeOrder, bonusActive: bonusActive, idOrders: idOrders });
+        res = await $api.post(`/history/order/add`,{idAddress: idAddress, idCart: cardId, timeOrder: timeOrder, bonus: bonus, idOrders: idOrders });
     }
 
     return res;
